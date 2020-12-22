e7d4b509cd362e20b86116218b54465bb83af647
dymafr <erwan1309@hotmail.com>
2020-12-22 12:02:16 +0100

    end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule, Routes } from "@angular/router";
-
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { UsersComponent } from "./users/users.component";
 import { UserComponent } from "./user/user.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { APP_ROUTES } from "./app.routes";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
+  imports: [BrowserModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HomepageComponent,
